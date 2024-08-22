be0f32faec5c7266e8242e600ccb4370c67f5f57
plony <124659740+plony@users.noreply.github.com>
2024-08-22 17:13:37 +0300

    time seres

--- EDA/analysis.py
+++ EDA/analysis.py
@@ -4,3 +4,11 @@ import matplotlib as plt
 import seaborn as sns
 data = pd.read_csv('benin-malanville.csv')
 print(data.describe())
+print(data.isnull().sum())
+print(data[data['GHI']<0].count())
+# Time Series Anysis
+plt.plot(data['GHI'])
+plt.xlabel('Time')
+plt.ylabel('GHI')
+plt.title('GHI over Time')
+plt.show()
