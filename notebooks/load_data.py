79ac51858f93c5ed48c1663f09e46fe439358c49
plony <124659740+plony@users.noreply.github.com>
2024-08-24 21:43:31 +0300

    edit

--- notebooks/load_data.py
+++ notebooks/load_data.py
@@ -1,15 +1,1 @@
 import pandas as pd
-import numpy as np
-import os, sys
-#os.getcwd()
-#sys.path.insert(0, os.path.abspath('../'))
-#from script.plot_correlation_heatmap
-import matplotlib.pyplot as plt
-import seaborn as sns
-#load the Dataset
-data = pd.read_csv('K:\data analysis\Solar_Radiation_Analysis\Data\sierraleone-bumbuna.csv')
-#summary statistics
-print(data.describe())
-#Data Quality Check
-print(data.isnull().sum())
-print(data[data['GHI'] <0].count())#check for negative values of GHI, DNI, DHI, ModA, ModB,WS, WSgust)
